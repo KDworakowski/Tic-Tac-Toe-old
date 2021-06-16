837f2c47458b19d993fcf86a0e2de183b41d84c2
Kacper Dworakowski <kacper.dworakowski@icloud.com>
2021-06-16 21:09:18 +0200

    Added simple API

--- app/models.py
+++ app/models.py
@@ -1,7 +1,15 @@
-from typing import Optional
+from typing import Optional, List
 
 from pydantic import BaseModel
 
 class GamePlayers(BaseModel):
     player1: Optional[str] = None
     player2: Optional[str] = None
+
+
+class RageQuit(BaseModel):
+    player_id: int
+
+class GameMove(BaseModel):
+    player_id: int
+    coordinate: list
